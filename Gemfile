ada69a2ab06ff03c3bacab0663515afe1b5e4185
Vaibhav Gupta <vaibhavg3023@gmail.com>
2024-04-08 16:00:23 +0530

    Add devise and bootstrap

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem "pg", "~> 1.1"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
+gem "devise"
+
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 gem "importmap-rails"
 
@@ -41,6 +43,8 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+
+gem 'pry'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", preload: true
+pin "popper", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.2/dist/umd/popper.min.js", preload: true
+# config/importmap.rb
+pin "@rails/ujs", to: "rails_ujs.js"
