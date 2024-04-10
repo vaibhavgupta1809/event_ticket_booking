bfe597d850e1f359bbc6506e472fdc4c44a17d10
Vaibhav Gupta <vaibhavg3023@gmail.com>
2024-04-10 17:22:08 +0530

    making views code reusable

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
+  skip_before_action :authenticate_user!, only: [:index]
   
 
   def index
